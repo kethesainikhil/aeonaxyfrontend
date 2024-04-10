ddc8e5b7b6533c15d668e040f7f34091ec334730
kethesainikhil <nikhilkethe007@gmail.com>
2024-04-11 00:04:51 +0530

    frontpage updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       {/* <Header /> */}
       <main className="flex items-center flex-col">
         <h1 className="text-black text-3xl py-10 font-bold">Welcome to dribble! </h1>
-        <button><Link to={"/home"}  className="bg-pink-500 px-10 py-4 text-white">click here</Link></button>
+        <button><Link to={"/home"}  className="bg-pink-500 px-10 py-4 text-white">Sign Up</Link></button>
         <Outlet />
       </main>
     </div>
